5bc0cd89e1edf82547e5c87c42830665a3183113
aiejvn <139830381+aiejvn@users.noreply.github.com>
2024-12-31 20:57:15 -0500

    minor backend work

--- api/index.py
+++ api/index.py
@@ -1,8 +1,28 @@
-from flask import Flask
 from translator import Translator
+import os
+from flask import Flask, render_template, request, redirect, url_for, Response
+import json
+from dotenv import load_dotenv
+import datetime
 
 app = Flask(__name__)
+translator = Translator()
 
 @app.route("/api/python")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/api/translate")
+def translate():
+    return f"<p>{translator.translate("Hello World!")}</p>"
+
+@app.route("/")
+def main():
+    return render_template(
+        "page.tsx",
+        title="translatera"
+        # url=os.getenv("URL")
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
