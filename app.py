bb6719f809ae19e30d0eca8e7314082617aa258c
aiejvn <139830381+aiejvn@users.noreply.github.com>
2025-01-01 14:12:45 -0500

    Moving files around + moved translator stuff

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from dotenv import load_dotenv, find_dotenv
-import os
-from typing import List
 import requests
+import os
+from flask import Flask, jsonify, render_template, request
 import json
-import re
+from dotenv import load_dotenv
+import datetime
+from flask_cors import CORS
 
 class Translator:
 
@@ -48,9 +50,44 @@ class Translator:
             return result['content']
         except Exception as e:
             return f"Could not find translated sentence. Error: {e}. Content: {json.loads(response.content)}"  
-        
-if __name__ == "__main__":
+
+def test_translator():
     translator = Translator()
     for i in range(10):
         text = translator.translate("Hello, world!")
         print("Iteration", i, ":", text)
+
+# from proj root:
+# flask run
+
+# env file contains environment (debug/production)
+
+app = Flask(__name__)
+CORS(app) # enables next js to make backend requests to flask
+translator = Translator()
+
+@app.route("/api/python")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/api/translate", methods=['GET'])
+def translate():
+    content = request.args.get("content")
+    input_language = request.args.get('input_language')
+    output_language = request.args.get('output_language')
+    
+    # print(input_language, output_language, content)
+    return jsonify({
+        'message':translator.translate(content, original=input_language, target=output_language)
+    })
+
+@app.route("/")
+def main():
+    return render_template(
+        "page.tsx",
+        title="translatera"
+        # url=os.getenv("URL")
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080, load_dotenv=True)
